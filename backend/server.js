26ecfee681b4e6234142b850be6ad4ca6691f4c9
Joanna Beryl J <joannaberyl.j.2025.cse@rajalakshmi.edu.in>
2026-09-08 16:45:41 +0530

    Initial portfolio project

---
+++ backend/server.js
@@ -0,0 +1,48 @@
+require("dotenv").config();
+const express = require("express");
+const mysql = require("mysql2");
+const cors = require("cors");
+const app = express();
+const PORT = 3000;
+app.use(cors());
+
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+
+db.connect((err) => {
+    if (err) {
+        console.log("MySQL connection failed:", err);
+    } else {
+        console.log("MySQL connected successfully! ✅");
+    }
+});
+
+// Home route
+app.get("/", (req, res) => {
+    res.send("Portfolio backend is running! 🚀");
+});
+
+// Projects route
+app.get("/projects", (req, res) => {
+    db.query("SELECT * FROM projects", (err, results) => {
+        if (err) {
+            console.log(err);
+            res.status(500).send("Database error");
+        } else {
+            res.json(results);
+        }
+    });
+});
+
+// Start server
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
