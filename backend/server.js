b2114d57c8e8cf959d72f620ff1efc0c03a63920
Joanna Beryl J <joannaberyl.j.2025.cse@rajalakshmi.edu.in>
2026-09-08 17:22:18 +0530

    Prepare backend for deployment

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const mysql = require("mysql2");
 const cors = require("cors");
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.use(cors());
 
 const db = mysql.createConnection({
